418db00bbabd98a582ae22e4220e69079fc95eeb
Alya Rees <alyarees@gmail.com>
2024-02-24 10:35:44 -0500

    adding changes up to task 11

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -17,7 +17,6 @@ if (validuser.length > 0) {
 }
 }
 
-
 const authenticatedUser = (username,password)=>{ 
     let authuser = users.filter((user)=>{
         return (user.username === username && user.password === password)//returns boolean
@@ -48,7 +47,7 @@ accessToken,username
     } 
     return res.status(200).json({message: "User successfully logged in!"});
     } else { 
-        return res.status(404).json({message: "Invalid log in credentials. Check username or password."});
+        return res.status(208).json({message: "Invalid log in credentials. Check username or password."});
     }});
 
 // Add a book review
@@ -62,11 +61,35 @@ if (review) {
     filtered_book['reviews'][reviewer] = review;
     books[isbn] = filtered_book;
 }
-return res.status(200).json(`Your review for ${isbn} has been submitted!`);
+res.send(`Your review for ${isbn} has been submitted!`);
   } else {
-    return res.status(404).json("Cannot submit review as unable to find book by ISBN.");
+    res.send("Cannot submit review as unable to find book by ISBN.");
   }
 });
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {    
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    const book = books[isbn];
+    
+    if (!username) {
+        return res.status(403).json({message: "User not authenticated."});
+    }
+
+    if (book) {
+        if (book.reviews.hasOwnProperty(username)) {
+
+            delete book.reviews[username];
+            return res.status(200).json({message: "Review has been successfully deleted."});
+        } else {
+            return res.status(404).json({message: "Cannot find book review."});
+        }
+    } else {
+        return res.status(404).json({message: "Invalid ISBN."});
+    }
+});
+  
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -31,14 +32,16 @@ if (samenameusers.length > 0) {
 }
 }
 
-
 // Get the book list available in the shop
-public_users.get('/',(req, res) => {
-  res.send(JSON.stringify(books,null,4));
+public_users.get('/books',(req, res) => {
+    const get_books = new Promise((resolve,reject)=>{
+        resolve(res.send(JSON.stringify({books},null,4)));
+    });
+    get_books.then(() => console.log("Promise for task 10 resolved!"));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/books/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     const book = books[isbn];
     if (book) {
@@ -49,8 +52,24 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
+public_users.get('/books/author/:author',function (req, res) {
+  const get_books_author = new Promise((resolve,reject)=>{
+
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn)=> {
+        if(books[isbn]["author"] === req.params.author) {
+booksbyauthor.push({"isbn": isbn,
+"title":books[isbn]["title"],
+"reviews":books[isbn]["reviews"]});
+resolve(res.send(JSON.stringify({booksbyauthor},null,4)));
+        }
+  });
+  reject(res.send("The mentioned author does not exist"));
+});
+
+//resume HERE! Use the discussion forum to help you write the code for the .then method for the console.logs and a catch method! Enjoy! You have learnt so much more in a shorter amount of time studying this way, keep it up!
+
   const authorBook = Object.values(books).filter (book => book.author === author);
   if (authorBook.length > 0 ) {
     return res.status(200).json(authorBook);
